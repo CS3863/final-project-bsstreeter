ac81f5d28d084db29b6b56bc8147a658ed5f9961
bsstreeter <124920658+bsstreeter@users.noreply.github.com>
2023-04-25 13:50:33 -0500

    rocket movement

---
+++ Moodetarium/Assets/Scripts/rocketController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class rocketController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController rocket;
+    public float speed = 5.0f;
+    public float tSpeed = 25.0f;
+    void Start()
+    {
+        rocket = GetComponent<CharacterController>();
+    }
+
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical); 
+        transform.Rotate(Vector3.up, tSpeed * horizontal * Time.deltaTime); 
+    }
+}
