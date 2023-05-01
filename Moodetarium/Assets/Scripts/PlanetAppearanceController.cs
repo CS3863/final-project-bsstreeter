e2e8e870eb4234a794c16eb3b1d8606cbc4f420a
zoedelap <zoe.a.delap@gmail.com>
2023-05-01 15:20:52 -0500

    gradual color change instead of instant

--- Moodetarium/Assets/Scripts/PlanetAppearanceController.cs
+++ Moodetarium/Assets/Scripts/PlanetAppearanceController.cs
@@ -7,16 +7,31 @@ public class PlanetAppearanceController : MonoBehaviour
     private Renderer planetRenderer;
     private float originalScale = 2.0f;
 
+    private float changeSpeed = 1.0f;
+
     void Start() {
         planetRenderer = GetComponent<Renderer>();
     }
 
     public void setColor(Color color) {
-       planetRenderer.material.SetColor("_BaseColor", color);
+    //    planetRenderer.material.SetColor("_BaseColor", color);
+        StartCoroutine(changeColorGradual(color));
+    }
+
+    private IEnumerator changeColorGradual(Color endColor)
+    {
+        Color startColor = planetRenderer.material.color;
+        float tick = 0f;
+        while (planetRenderer.material.color != endColor)
+        {
+            tick += Time.deltaTime * changeSpeed;
+            planetRenderer.material.SetColor("_BaseColor", Color.Lerp(startColor, endColor, tick) );
+            yield return null;
+        }
     }
 
     public void setSize(float scaleValue) {
         scaleValue = originalScale * scaleValue;
         gameObject.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
-    }
+    }   
 }
