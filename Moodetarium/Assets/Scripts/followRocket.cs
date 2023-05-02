95fe6cc3351feebf78ca23a215138cfb06612e22
zoedelap <zoe.a.delap@gmail.com>
2023-05-02 13:13:14 -0500

    camera mechanics fixed
    still some jitter on turns, but it will be workable

--- Moodetarium/Assets/Scripts/followRocket.cs
+++ Moodetarium/Assets/Scripts/followRocket.cs
@@ -5,32 +5,33 @@ using UnityEngine;
 public class followRocket : MonoBehaviour
 {
     public GameObject rocket;
-//    public float camMotionSpeed = 50f;
     public float camRotationSpeed = 50f;
-    private Vector3 offset;
+    private Vector3 posOffset;
+    private Vector3 lookOffset;
     public float cameraLookAbove;
 
     void Start()
     {
-        offset = transform.position - rocket.transform.position;
-        transform.position = rocket.transform.position + (transform.forward * offset.z) + (rocket.transform.up * offset.y);
-        transform.LookAt(rocket.transform.position + new Vector3(0, cameraLookAbove, 0));
+        posOffset = transform.position - rocket.transform.position;
+        lookOffset = new Vector3(0, cameraLookAbove, 0);
+        transform.position = rocket.transform.position + (transform.forward * posOffset.z) + (rocket.transform.up * posOffset.y);
+        transform.LookAt(rocket.transform.position + lookOffset);
     }
 
     private void UpdateCameraPosition()
     {
         //Move
-        Vector3 newCamPosition = rocket.transform.position + (rocket.transform.forward * offset.z) + (rocket.transform.up * offset.y);
+        Vector3 newCamPosition = rocket.transform.position + (rocket.transform.forward * posOffset.z) + (rocket.transform.up * posOffset.y);
         transform.position = newCamPosition;
 
         //Rotate
-        Quaternion newCamRotation = Quaternion.LookRotation(rocket.transform.position - transform.position);
+        Quaternion newCamRotation = Quaternion.LookRotation(rocket.transform.position + lookOffset - transform.position);
         newCamRotation = Quaternion.Slerp(transform.rotation, newCamRotation, camRotationSpeed * Time.smoothDeltaTime); //spherical lerp smoothing
         transform.rotation = newCamRotation;
 
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         UpdateCameraPosition();
     }
