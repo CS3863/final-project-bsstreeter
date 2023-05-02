b8983ed413bf1fbf4413c4d2efe7fcf28daec852
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-05-02 18:33:19 -0500

    removed uneccessary commended code

--- Moodetarium/Assets/Scripts/rocketController.cs
+++ Moodetarium/Assets/Scripts/rocketController.cs
@@ -18,25 +18,4 @@ public class rocketController : MonoBehaviour
     {
         transform.RotateAround(target.transform.position, Vector3.up, degreesPerSecond * Time.deltaTime);
     }
-    /*// Start is called before the first frame update
-    // public CharacterController rocket;
-    public float speed = 5.0f;
-    public float tSpeed = 25.0f;
-    
-    void Start()
-    {
-        // rocket = GetComponent<CharacterController>();
-    }
-
-    
-    
-    // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical); 
-        transform.Rotate(Vector3.up, tSpeed * horizontal * Time.deltaTime); 
-    }*/
 }
